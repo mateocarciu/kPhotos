f97618672252919412f4257695260af918892ea1
Mateo <carciu98@gmail.com>
2025-04-23 22:15:17 +0200

    fix(api): reorganize routes in api

--- app/components/drive/PhotoCard.vue
+++ app/components/drive/PhotoCard.vue
@@ -5,7 +5,7 @@
         <UIcon name="i-heroicons-arrow-path" class="h-6 w-6 animate-spin text-gray-400" />
       </div>
 
-      <NuxtImg :src="`/api/drives/thumbnail?drive_id=${driveId}&file_id=${file.id}&width=400&height=400`" :alt="file.name" class="h-64 w-full object-contain transition-opacity duration-300" :class="{ 'opacity-0': !loaded, 'opacity-100': loaded }" @load="$emit('loaded', file.id)" />
+      <NuxtImg :src="`/api/drives/files/thumbnail?drive_id=${driveId}&file_id=${file.id}&width=400&height=400`" :alt="file.name" class="h-64 w-full object-contain transition-opacity duration-300" :class="{ 'opacity-0': !loaded, 'opacity-100': loaded }" @load="$emit('loaded', file.id)" />
 
       <div class="absolute bottom-0 w-full truncate bg-black/50 p-2 text-xs text-white opacity-0 transition-opacity group-hover:opacity-100">
         {{ file.name }}
