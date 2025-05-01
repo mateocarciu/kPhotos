d3aaf9d6d16265c02425edfd663e462d526e888c
Mateo <carciu98@gmail.com>
2025-05-01 15:19:20 +0200

    fix(files/detail): adjust modal

--- app/components/modals/UIFilesDetail.vue
+++ app/components/modals/UIFilesDetail.vue
@@ -1,9 +1,9 @@
 <template>
-  <UModal v-model:open="isOpen" :ui="{ content: 'sm:max-w-7xl' }" :title="file.name" :description="`File ${file.extension_type.toUpperCase()}`" @close="emit('close', false)">
+  <UModal v-model:open="isOpen" :ui="{ content: modalContentClass }" :title="file.name" :description="`File ${file.extension_type.toUpperCase()}`" @close="emit('close', false)">
     <template #body>
       <div class="flex flex-col items-center gap-6 bg-transparent p-4">
-        <div class="relative w-full rounded-xl">
-          <NuxtImg v-if="imageUrl" :src="imageUrl" :alt="file.name" class="max-h-[calc(80vh-150px)] w-full object-contain opacity-0 transition-opacity duration-500" @load="onImageLoad" />
+        <div class="relative flex w-full items-center justify-center rounded-xl">
+          <NuxtImg v-if="imageUrl" ref="imageRef" :src="imageUrl" :alt="file.name" class="max-h-[calc(80vh-150px)] rounded-xl opacity-0 transition-opacity duration-500" @load="onImageLoad" />
 
           <div v-if="loading" class="inset-0 flex items-center justify-center">
             <div class="h-16 w-16 animate-spin rounded-full border-4 border-gray-300 border-t-(--ui-primary)" />
@@ -14,7 +14,7 @@
           {{ showDetails ? 'Hide details' : 'Show details' }}
         </UButton>
 
-        <div v-if="showDetails" class="grid w-full grid-cols-1 gap-4 text-sm text-gray-800 sm:grid-cols-2 dark:text-gray-200">
+        <div v-if="showDetails" class="grid w-full grid-cols-1 gap-4 text-sm text-gray-800 dark:text-gray-200">
           <div class="flex flex-col">
             <span class="text-xs text-gray-500 dark:text-gray-400">Name</span>
             <span class="font-medium">{{ file.name }}</span>
@@ -56,12 +56,15 @@ const isOpen = ref(false)
 const loading = ref(true)
 const imageUrl = ref('')
 const showDetails = ref(false)
+const imageRef = ref<HTMLImageElement | null>(null)
+const modalContentClass = ref()
 
 watchEffect(() => {
   if (props.file) {
     loading.value = true
     imageUrl.value = getImageUrl(props.file)
     showDetails.value = false
+    modalContentClass.value = 'sm:max-w-sm'
   }
 })
 
@@ -77,5 +80,15 @@ function onImageLoad(e: Event) {
   loading.value = false
   const img = e.target as HTMLImageElement
   img.style.opacity = '1'
+
+  if (img.naturalWidth > 1800) {
+    modalContentClass.value = 'sm:max-w-7xl'
+  } else if (img.naturalWidth > 1200) {
+    modalContentClass.value = 'sm:max-w-5xl'
+  } else if (img.naturalWidth > 800) {
+    modalContentClass.value = 'sm:max-w-3xl'
+  } else {
+    modalContentClass.value = 'sm:max-w-fit'
+  }
 }
 </script>
