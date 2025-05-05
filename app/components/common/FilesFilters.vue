4668a034be0b820a69a6e44d7bc3b6923737afd6
Mateo <carciu98@gmail.com>
2025-05-05 23:05:59 +0200

    fix(filters): open filters with popover

--- app/components/common/FilesFilters.vue
+++ app/components/common/FilesFilters.vue
@@ -1,34 +1,32 @@
 <template>
-  <UTooltip text="Filters">
-    <UButton icon="i-heroicons-adjustments-horizontal" class="fixed bottom-6 left-1/2 z-50 -translate-x-1/2 shadow-xl" size="lg" color="primary" variant="solid" @click="showFilters = !showFilters" />
-  </UTooltip>
-  <Transition enter-active-class="transition transform duration-300" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="transition transform duration-200" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-    <div v-if="showFilters" class="fixed bottom-[5rem] left-1/2 z-50 flex -translate-x-1/2 flex-col items-center gap-4 rounded-xl border border-gray-200 bg-white p-3 shadow-xl backdrop-blur sm:flex-row dark:border-gray-800 dark:bg-gray-900">
-      <USelectMenu v-model="filters.types" multiple :items="fileTypes" :search-input="false" placeholder="Types" class="w-32" @update:model-value="updateFilters" />
-      <UPopover>
-        <UButton color="neutral" variant="outline" icon="i-lucide-calendar" disabled>
-          <template v-if="modelValue.start">
-            <template v-if="modelValue.end"> {{ df.format(modelValue.start.toDate(getLocalTimeZone())) }} - {{ df.format(modelValue.end.toDate(getLocalTimeZone())) }} </template>
-          </template>
-          <template v-else> Pick a date </template>
-        </UButton>
+  <UPopover>
+    <UButton icon="i-heroicons-adjustments-horizontal" class="fixed bottom-6 left-1/2 z-50 -translate-x-1/2 shadow-xl" size="lg" color="primary" variant="solid" />
+    <template #content>
+      <div class="fixed bottom-[5rem] left-1/2 z-50 flex -translate-x-1/2 flex-col items-center gap-4 rounded-xl border border-gray-200 bg-white p-3 shadow-xl backdrop-blur sm:flex-row dark:border-gray-800 dark:bg-gray-900">
+        <USelectMenu v-model="filters.types" multiple :items="fileTypes" :search-input="false" placeholder="Types" class="w-32" @update:model-value="updateFilters" />
+        <UPopover class="w-32">
+          <UButton color="neutral" variant="outline" icon="i-lucide-calendar" disabled>
+            <template v-if="modelValue.start">
+              <template v-if="modelValue.end"> {{ df.format(modelValue.start.toDate(getLocalTimeZone())) }} - {{ df.format(modelValue.end.toDate(getLocalTimeZone())) }} </template>
+            </template>
+            <template v-else> Pick a date </template>
+          </UButton>
 
-        <template #content>
-          <UCalendar v-model="modelValue" :number-of-months="2" range />
-        </template>
-      </UPopover>
-      <USelectMenu v-model="filters.modified_at" :items="modifiedDateOptions" :search-input="false" placeholder="Modified" class="w-32" @update:model-value="updateFilters" />
-      <USelectMenu v-model="filters.order_by" :items="orderOptions" option-attribute="label" :search-input="false" placeholder="Order by" class="w-32" @update:model-value="updateFilters" />
-      <USelectMenu v-model="filters.order_dir" :items="['asc', 'desc']" placeholder="Direction" :search-input="false" class="w-32" @update:model-value="updateFilters" />
-      <UButton icon="i-heroicons-trash" color="error" variant="soft" @click="resetFilters" />
-    </div>
-  </Transition>
+          <template #content>
+            <UCalendar v-model="modelValue" :number-of-months="2" range />
+          </template>
+        </UPopover>
+        <USelectMenu v-model="filters.modified_at" :items="modifiedDateOptions" :search-input="false" placeholder="Modified" class="w-32" @update:model-value="updateFilters" />
+        <USelectMenu v-model="filters.order_by" :items="orderOptions" option-attribute="label" :search-input="false" placeholder="Order by" class="w-32" @update:model-value="updateFilters" />
+        <USelectMenu v-model="filters.order_dir" :items="['asc', 'desc']" placeholder="Direction" :search-input="false" class="w-32" @update:model-value="updateFilters" />
+        <UButton icon="i-heroicons-trash" color="error" variant="soft" @click="resetFilters" />
+      </div>
+    </template>
+  </UPopover>
 </template>
 
 <script setup lang="ts">
 import { DateFormatter, getLocalTimeZone } from '@internationalized/date'
-
-const showFilters = ref(false)
 
 const df = new DateFormatter('en-US', {
   dateStyle: 'medium'
